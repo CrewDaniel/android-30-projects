7cb6ff252af061a5d89df19b001e479964bdb698
CrewDaniel <alaskay@naver.com>
2021-07-07 14:03:08 +0900

    MOD aoppart2chapter02
    lotto number picker complete

--- aoppart2chapter02/app/src/main/java/fastcampus/aop/part2_chapter02/MainActivity.kt
+++ aoppart2chapter02/app/src/main/java/fastcampus/aop/part2_chapter02/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.NumberPicker
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
@@ -35,7 +36,7 @@ class MainActivity : AppCompatActivity() {
             findViewById(R.id.textView4),
             findViewById(R.id.textView5),
             findViewById(R.id.textView6)
-            )
+        )
     }
 
     private var didRun = false
@@ -51,11 +52,32 @@ class MainActivity : AppCompatActivity() {
 
         initRunButton()
         initAddButton()
+        initClearButton()
+    }
+
+    private fun initClearButton() {
+        clearButton.setOnClickListener {
+            pickNumberSet.clear()
+            numberTextViewList.forEach {
+                it.isVisible = false
+            }
+            didRun = false
+        }
     }
 
     private fun initRunButton() {
         runButton.setOnClickListener {
             val list = getRandomNumber()
+
+            didRun = true
+
+            list.forEachIndexed { index, num ->
+                val textView = numberTextViewList[index]
+                textView.text = num.toString()
+                textView.isVisible = true
+
+                setNumberBackground(num, textView)
+            }
 
             Log.d("MainActivity", "$list")
         }
@@ -81,15 +103,34 @@ class MainActivity : AppCompatActivity() {
 
             val textView = numberTextViewList[pickNumberSet.size]
             textView.isVisible = true
-            textView.
+            textView.text = numberPicker.value.toString()
+
+            setNumberBackground(numberPicker.value, textView)
+
+            pickNumberSet.add(numberPicker.value)
         }
     }
 
     private fun getRandomNumber(): List<Int> {
-        val numberList = MutableList(45) { it + 1 }
+        val numberList = mutableListOf<Int>().apply {
+            for (i in 1..45) {
+                if (pickNumberSet.contains(i)) continue
+                this.add(i)
+            }
+        }
 
         numberList.shuffle()
 
-        return numberList.subList(0, 6).sorted()
+        return (pickNumberSet.toList() + numberList.subList(0, 6 - pickNumberSet.size)).sorted()
+    }
+
+    private fun setNumberBackground(number: Int, textView: TextView) {
+        when(number) {
+            in 1..10 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_yellow)
+            in 11..20 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_blue)
+            in 21..30 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_red)
+            in 31..40 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_gray)
+            else -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_green)
+        }
     }
 }
