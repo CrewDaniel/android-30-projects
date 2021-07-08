7c9d94b21f9680fb45c0019299affba36c4d6056
CrewDaniel <alaskay@naver.com>
2021-07-08 14:59:51 +0900

    MOD aoppart2chapter03
    diary complete

---
+++ aoppart2chapter03/app/src/main/java/fastcampus/aop_part2_chapter03/DiaryActivity.kt
@@ -0,0 +1,43 @@
+package fastcampus.aop_part2_chapter03
+
+import android.content.Context
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import androidx.core.widget.addTextChangedListener
+
+class DiaryActivity: AppCompatActivity() {
+
+    private val handler = Handler(Looper.getMainLooper())
+
+    private val diaryEditText: EditText by lazy {
+        findViewById(R.id.diaryEditText)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary)
+
+        val detailPreferences = getSharedPreferences("diary", Context.MODE_PRIVATE)
+        diaryEditText.setText(detailPreferences.getString("detail", ""))
+
+        val runnable = Runnable {
+            getSharedPreferences("diary", Context.MODE_PRIVATE).edit {
+                putString("detail", diaryEditText.text.toString())
+            }
+            Log.d("DiaryActivity", "Saved: ${diaryEditText.text}")
+        }
+
+        diaryEditText.addTextChangedListener {
+            Log.d("DiaryActivity", "TextChanged: $it")
+            handler.removeCallbacks(runnable)
+            handler.postDelayed(runnable, 500)
+        }
+
+
+    }
+}
